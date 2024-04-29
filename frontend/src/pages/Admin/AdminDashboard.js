441b86d9b8ff42e2772813715975d02da60772f8
david Murage <davidmurage415@gmail.com>
2024-04-29 14:40:12 +0300

    added AdminMenu.js and updated AdminDashboard.js

--- frontend/src/pages/Admin/AdminDashboard.js
+++ frontend/src/pages/Admin/AdminDashboard.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import AdminMenu from '../../components/Layout/AdminMenu'
+import { useAuth } from '../../context/auth'
 
 const AdminDashboard = () => {
+    const {auth} = useAuth();
   return (
     <Layout>
-      <h1>AdminDashboard page</h1>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-13'>
+            <AdminMenu/>
+          </div>
+          <div className='col-mid-9'>
+            <div className='card w-75 p-3'>
+                <h1>Admin Name : {auth?.user?.name}</h1>
+                <h1>Admin Email : {auth?.user?.email}</h1>
+                <h1>Admin Contact : {auth?.user?.phone}</h1>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
