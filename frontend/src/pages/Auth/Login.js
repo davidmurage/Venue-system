4a7cd54c9218120ba5ddc882b0cf71a6cff5d148
david Murage <davidmurage415@gmail.com>
2024-08-06 16:14:57 +0300

    updated Login.js

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // function to handle submit
+  // function to handle submit btn
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
